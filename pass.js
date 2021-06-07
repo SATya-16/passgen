062aeee90be651a1a5938fe0bf8906d04f1413b2
SATya-16 <84380313+SATya-16@users.noreply.github.com>
2021-06-07 22:54:49 +0530

    password

---
+++ pass.js
@@ -0,0 +1,21 @@
+
+    
+    let passfield = document.getElementById('password');
+    function generate(){
+        let strings = 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890!@#$%^&*()-=+{}[]\|\|;:,.<>$%@%#^&$#%::;;,./?/`~';
+
+        let password = '';
+
+        for(let i = 0; i < strings.length; i++){
+            let randomNum = Math.floor(Math.random() * strings.length);
+            password += strings.substr(randomNum,randomNum +1);
+            let finalpass = password.substr(0, document.getElementById('length').value);
+            passfield.value = finalpass;
+        }
+    }
+
+   
+    let btncopy=document.getElementById("btn2");
+    btncopy.onclick=function(){
+        passfield.select("password");
+    };
